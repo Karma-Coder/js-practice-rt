f587abf7a71d433952ff96ff2d859b0eb0ad7445
Rahult-07 <rahulyodkar78@gmail.com>
2025-02-18 17:59:14 +0000

    changes

---
+++ basics.js/datatype.js
@@ -0,0 +1,15 @@
+const accountId = 144553
+let accountEmail = "hitesh@gmail.com"
+var accountPassword = "12345"
+accountCity= "Jaipur"
+
+//accountId = 2 // not allowed 
+
+accountEmail = "hc@hc.com"
+accountPassword = "4562"
+accountCity = "Bengluru"
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword,accountCity ]) 
+
+
+
