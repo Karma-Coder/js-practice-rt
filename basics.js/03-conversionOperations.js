8d4aed9bc18a54c96aa32797660f7475535da8b1
Rahult-07 <rahulyodkar78@gmail.com>
2025-03-04 14:37:57 +0000

    conversion and datatypes

--- basics.js/03-conversionOperations.js
+++ basics.js/03-conversionOperations.js
@@ -1,1 +1,27 @@
+let score = true
 
+// //console.log(typeof score);
+// //console.log(typeof (score));
+
+ let valueInNumber = Number(score)
+// //console.log(typeof valueInNumber)
+// //console.log(valueInNumber)
+
+
+// when we print a number "33" => 33 getting a response 
+// "33abc" => NaN (Not a Number) 
+// true => 1 ,false => 0
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => true ,0 => false
+//"" => false
+// "Hitesh" => true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
---
+++ basics.js/datatype.js
@@ -0,0 +1,16 @@
+const accountId = 144553
+let accountEmail = "hitesh@gmail.com"
+var accountPassword = "12345"
+accountCity= "Jaipur"
+let accountState;
+
+//accountId = 2 // not allowed 
+
+accountEmail = "hc@hc.com"
+accountPassword = "4562"
+accountCity = "Bengluru"
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword,accountCity, accountState ]) 
+
+
+
